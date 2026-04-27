ac267199117a9e691c544b44b9ef80ab6d4afc0b
Mohammad Bilal <111408918+mohdbilal123456@users.noreply.github.com>
2026-04-27 16:35:02 +0530

    Update api.js

--- chat-ui/src/config/axios/api.js
+++ chat-ui/src/config/axios/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-const BASE_URL = 'http://localhost:5000/api'
-// const BASE_URL = 'https://code-blog-1-5m0t.onrender.com/api'
+// const BASE_URL = 'http://localhost:5000/api'
+const BASE_URL = 'https://chat-app-chat-api.onrender.com/api'
 /* ================================
 TOKEN HELPERS
 ================================ */
